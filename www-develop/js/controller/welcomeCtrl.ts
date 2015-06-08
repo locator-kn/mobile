1797e06f5f72960329a75b51fb598b12ec521f4e
T1m1 <Ruprecht.t@gmx.de>
2015-06-08 18:07:19 +0200

    add logo to navbar

--- www-develop/js/controller/welcomeCtrl.ts
+++ www-develop/js/controller/welcomeCtrl.ts
@@ -2,10 +2,13 @@ module Controller {
     export class WelcomeCtrl {
 
         constructor(private $scope, private $rootScope, private $element) {
+            // add logo to navbar
+            $scope.navTitle= '<img src="./images/locator-logo.png" class="logo"/>';
+
+            // set height (workaround) for each button
             var elementHeigth = angular.element(this.$element).height();
             var heightTop = 42.9805;
             var heightBottom = 48.9865;
-            console.log(angular.element(".height-3"));
             angular.element(".height-3").css({'height': (elementHeigth-(heightBottom+heightTop)) / 3 +"px" });
         }
 
