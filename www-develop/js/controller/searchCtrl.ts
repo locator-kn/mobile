4ca4afc4e509cade2db00413e1fa2cbb65bffe88
T1m1 <Ruprecht.t@gmx.de>
2015-06-10 16:47:02 +0200

    add accomodation view

--- www-develop/js/controller/searchCtrl.ts
+++ www-develop/js/controller/searchCtrl.ts
@@ -12,14 +12,14 @@ module Controller {
                 {value: 1, title: "1"},
                 {value: 2, title: "2"},
                 {value: 3, title: "3"},
-                {value: 4, title: "3+"},
+                {value: 4, title: "3+"}
             ];
             this.availablePersons = [
                 {value: 1, title: "1"},
                 {value: 2, title: "2"},
                 {value: 3, title: "3"},
                 {value: 4, title: "4"},
-                {value: 5, title: "4+"},
+                {value: 5, title: "4+"}
             ];
         }
 
