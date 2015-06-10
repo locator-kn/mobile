e98e6b55a6bee2b750950ac3f84c0d9de1b5f45e
T1m1 <Ruprecht.t@gmx.de>
2015-06-10 16:27:22 +0200

    add days to search view

--- www-develop/js/controller/searchCtrl.ts
+++ www-develop/js/controller/searchCtrl.ts
@@ -4,12 +4,22 @@ module Controller {
         selectedDays:number = 1;
         availableDays:any = [];
 
+        selectedPersons:number = 1;
+        availablePersons:any = [];
+
         constructor(private $scope, private $rootScope, private $element, private $state) {
             this.availableDays = [
                 {value: 1, title: "1"},
                 {value: 2, title: "2"},
                 {value: 3, title: "3"},
                 {value: 4, title: "3+"},
+            ];
+            this.availablePersons = [
+                {value: 1, title: "1"},
+                {value: 2, title: "2"},
+                {value: 3, title: "3"},
+                {value: 4, title: "4"},
+                {value: 5, title: "4+"},
             ];
         }
 
