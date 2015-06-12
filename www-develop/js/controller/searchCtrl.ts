15af467cc555b564e5479b2088792f6561ba2ef1
T1m1 <Ruprecht.t@gmx.de>
2015-06-12 13:29:14 +0200

    implement get cities function

--- www-develop/js/controller/searchCtrl.ts
+++ www-develop/js/controller/searchCtrl.ts
@@ -7,7 +7,9 @@ module Controller {
         selectedPersons:number = 1;
         availablePersons:any = [];
 
-        constructor(private $scope, private $rootScope, private $element, private $state) {
+        tripCities:any = [];
+
+        constructor(private $scope, private $rootScope, private $element, private $state, private DataService) {
             this.availableDays = [
                 {value: 1, title: "1"},
                 {value: 2, title: "2"},
@@ -21,6 +23,11 @@ module Controller {
                 {value: 4, title: "4"},
                 {value: 5, title: "4+"}
             ];
+
+            this.DataService.getCities()
+                .then(result => {
+                    this.tripCities = result.data;
+                });
         }
 
         static controllerId:string = "SearchCtrl";
