51a2136b9a6192a6ff3cf23b6450ef6dae8fbfa5
T1m1 <Ruprecht.t@gmx.de>
2015-06-13 10:56:30 +0200

    refactor available days and persons

--- www-develop/js/controller/searchCtrl.ts
+++ www-develop/js/controller/searchCtrl.ts
@@ -11,21 +11,8 @@ module Controller {
         city:string;
 
         constructor(private $scope, private $rootScope, private $element, private $state, private DataService) {
-            // TODO - outsource in service
-            this.availableDays = [
-                {value: 1, title: "1"},
-                {value: 2, title: "2"},
-                {value: 3, title: "3"},
-                {value: 4, title: "3+"}
-            ];
-            this.availablePersons = [
-                {value: 1, title: "1"},
-                {value: 2, title: "2"},
-                {value: 3, title: "3"},
-                {value: 4, title: "4"},
-                {value: 5, title: "4+"}
-            ];
-
+            this.availableDays = this.DataService.getAvailableDays();
+            this.availablePersons = this.DataService.getAvailablePersons();
         }
 
         searchCity(searchFilter) {
--- www-develop/js/service/dataService.ts
+++ www-develop/js/service/dataService.ts
@@ -2,6 +2,21 @@ module Service {
     export class DataService {
 
         dataCitiesCache;
+        availableDays = [
+            {value: 1, title: "1"},
+            {value: 2, title: "2"},
+            {value: 3, title: "3"},
+            {value: 4, title: "3+"}
+        ];
+
+        availablePersons = [
+            {value: 1, title: "1"},
+            {value: 2, title: "2"},
+            {value: 3, title: "3"},
+            {value: 4, title: "4"},
+            {value: 5, title: "4+"}
+        ];
+
 
         constructor(private $http, private basePath, private CacheFactory) {
             this.dataCitiesCache = CacheFactory.createCache('dataCities');
@@ -10,6 +25,15 @@ module Service {
         getCities() {
             return this.$http.get(this.basePath + '/data/cities', {cache: this.dataCitiesCache});
         }
+
+        getAvailableDays() {
+            return this.availableDays;
+        }
+
+        getAvailablePersons() {
+            return this.availablePersons;
+        }
+
 
         static serviceId:string = "DataService";
     }
