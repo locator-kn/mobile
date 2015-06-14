6c2672d49f683d267075a75b14b0d563a476c79a
T1m1 <Ruprecht.t@gmx.de>
2015-06-14 15:06:18 +0200

    tests

--- www-develop/js/controller/searchResultCtrl.ts
+++ www-develop/js/controller/searchResultCtrl.ts
@@ -3,9 +3,12 @@ module Controller {
         results:any;
 
 
-        constructor(private $scope, private $rootScope, private ResultService) {
-            this.results = ResultService.getResults();
+        constructor(private $scope, private $rootScope, private ResultService, private $element) {
+            // css test TOOD: remove
+            var elementWidth = angular.element(this.$element).width();
+            angular.element(".tmpImageWidth").css({'width': elementWidth + "px"})
 
+            this.results = ResultService.getResults();
             $rootScope.$on('newSearchResults', () => {
                 this.results = ResultService.getResults();
             });
