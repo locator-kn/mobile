ed5aca74daf5fe313a39617af21d9fb813a52968
T1m1 <Ruprecht.t@gmx.de>
2015-06-16 19:11:10 +0200

    impl. trip controller

--- www-develop/js/controller/tripCtrl.ts
+++ www-develop/js/controller/tripCtrl.ts
@@ -4,11 +4,19 @@ module Controller {
         trip:any = {};
         tripId:string;
 
-        constructor(private $scope, private $rootScope, private $stateParams, private $state, private SearchService) {
+        moods:any = [];
+
+        constructor(private $scope, private $rootScope, private $stateParams, private $state, private SearchService, private DataService) {
             // get trip by id from state param
             SearchService.getTripById(this.$stateParams.tripId).then((result) => {
-                this.trip = result.data;
+                this.trip = result.data[0];
+                $rootScope.navTitle= this.trip.title;
             });
+
+            this.DataService.getAvailableMoods().then((result) => {
+                this.moods = result.data;
+            });
+
 
         }
 
