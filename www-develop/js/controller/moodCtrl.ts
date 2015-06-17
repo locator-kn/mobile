3ca5a9015e54ce5db0168fa456bccb1b9ddfedcf
T1m1 <Ruprecht.t@gmx.de>
2015-06-17 20:30:36 +0200

    implement max select of three elements

--- www-develop/js/controller/moodCtrl.ts
+++ www-develop/js/controller/moodCtrl.ts
@@ -2,6 +2,7 @@ module Controller {
     export class MoodCtrl {
 
         moods:any = {};
+        selected:any = [];
 
         constructor(private DataService, private SearchService) {
             this.DataService.getAvailableMoods().then((result) => {
@@ -11,6 +12,26 @@ module Controller {
 
         setMoods(moods) {
             this.SearchService.setMoods(moods);
+        }
+
+        sync(bool, item) {
+            if (bool) {
+                // add item
+                this.selected.push(item);
+                // if we have gone over maxItems:
+                if (this.selected.length > 3) {
+                    //remove oldest item
+                    this.selected[0].checked = false;
+                    this.selected.splice(0,1);
+                }
+            } else {
+                // remove item
+                for (var i=0 ; i < this.selected.length; i++) {
+                    if (this.selected[i] === item){
+                        this.selected.splice(i,1);
+                    }
+                }
+            }
         }
 
         static
