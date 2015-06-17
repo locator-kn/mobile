2efee149ed94b85550482d73f79235f684f859a3
T1m1 <Ruprecht.t@gmx.de>
2015-06-18 01:00:51 +0200

    get user infos

--- www-develop/js/controller/tripCtrl.ts
+++ www-develop/js/controller/tripCtrl.ts
@@ -4,20 +4,27 @@ module Controller {
         trip:any = {};
         tripId:string;
 
+        user:any = {};
+
         moods:any = [];
 
-        // TODO remove
+        // TODO remove variable
         witdh;
 
-        constructor(private $rootScope, private $element, private $stateParams, private SearchService, private DataService) {
-            // TODO remove
-            this.witdh =  angular.element(this.$element).width();
+        constructor(private $rootScope, private $element, private $stateParams, private SearchService, private DataService, private UserService) {
+            // TODO remove line
+            this.witdh = angular.element(this.$element).width();
 
             // get trip by id from state param
             SearchService.getTripById(this.$stateParams.tripId).then((result) => {
                 this.trip = result.data[0];
-                $rootScope.navTitle = this.trip.title;
+
+                this.UserService.getUser(this.trip.userid)
+                    .then(result => {
+                        this.user = result.data;
+                    });
             });
+
 
             this.DataService.getAvailableMoods().then((result) => {
                 this.moods = result.data;
