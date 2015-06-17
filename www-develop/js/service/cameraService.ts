3efd3983d8d63fa7e55e5c5d0a18e3a5cc743807
T1m1 <Ruprecht.t@gmx.de>
2015-06-17 10:34:18 +0200

    implement camera service to get picture

---
+++ www-develop/js/service/cameraService.ts
@@ -0,0 +1,25 @@
+module Service {
+    export class CameraService {
+
+
+        constructor(private $q) {
+        }
+
+
+        getPicture() {
+            var q = this.$q.defer();
+
+            navigator.camera.getPicture((result) => {
+                // Do any magic we need
+                q.resolve(result);
+            }, (err) => {
+                q.reject(err)
+            });
+
+            return q.promise;
+        }
+
+        static
+            serviceId:string = "CameraService";
+    }
+}
