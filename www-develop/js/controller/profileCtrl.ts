ac8070f6a64567a1095b1aaf1ea2f3545e416073
T1m1 <Ruprecht.t@gmx.de>
2015-06-18 16:23:35 +0200

    impl. profile controller

--- www-develop/js/controller/profileCtrl.ts
+++ www-develop/js/controller/profileCtrl.ts
@@ -1,9 +1,17 @@
 module Controller {
     export class ProfileCtrl {
 
+        user:any = {};
 
-        constructor(private UserService) {
+        constructor(private UserService, private $stateParams) {
+
+            this.UserService.getMe($stateParams.profileId)
+                .then(result => {
+                    this.user = result.data;
+                });
+
         }
+
 
 
         static
