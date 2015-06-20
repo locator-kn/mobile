148205c55752f55665b0ff38a6c1f6e9faf87979
T1m1 <Ruprecht.t@gmx.de>
2015-06-20 21:53:55 +0200

    implement function to change state depends on which tab the controller is used

--- www-develop/js/controller/tripOverviewCtrl.ts
+++ www-develop/js/controller/tripOverviewCtrl.ts
@@ -7,25 +7,29 @@ module Controller {
         // TODO: to test
         witdh;
 
-        constructor(private $rootScope, private ResultService, private $stateParams, private TripService, private $element, private DataService, private  $ionicLoading) {
+        searchView:boolean;
+
+        constructor(private $rootScope, private $state, private ResultService, private $stateParams, private TripService, private $element, private DataService, private  $ionicLoading) {
             // css test TOOD: remove
             var elementWidth = angular.element(this.$element).width();
             this.witdh = elementWidth;
             angular.element(".tmpImageWidth").css({'width': elementWidth + "px"});
 
             // if no user id is committed -> controller used for search results
-            if(!$stateParams.userId){
+            if (!$stateParams.userId) {
+                this.searchView = true;
                 this.results = ResultService.getResults();
                 $rootScope.$on('newSearchResults', () => {
                     this.results = ResultService.getResults();
                     this.$ionicLoading.hide();
                 });
             } else {
+                this.searchView = false;
                 // if user id is comitted as state param user id -> get all trips of user
                 this.TripService.getTripsByUser($stateParams.userId)
                     .then(result => {
-                    this.results = result.data;
-                });
+                        this.results = result.data;
+                    });
             }
 
             this.DataService.getAvailableMoods().then((result) => {
@@ -33,6 +37,21 @@ module Controller {
             });
 
         }
+
+        showTripDetail = (tripId, userId) => {
+            if (this.searchView) {
+                var state = 'tab.search-result-trip';
+            } else {
+                var state = 'tab.profile-trip';
+            }
+
+            this.$state.go(state, {
+                tripId: tripId,
+                userId: userId
+            });
+
+            //ui-sref="tab.search-result-trip({tripId:result._id})"
+        };
 
         static
             controllerId:string = "TripOverviewCtrl";
