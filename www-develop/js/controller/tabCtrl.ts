713e61b3b6ee8cc286d9cd64d416d3749d2b223a
T1m1 <Ruprecht.t@gmx.de>
2015-06-20 20:29:22 +0200

    bugfix: add state change -> better because no caching of user id

--- www-develop/js/controller/tabCtrl.ts
+++ www-develop/js/controller/tabCtrl.ts
@@ -1,13 +1,20 @@
 module Controller {
     export class TabCtrl {
 
-        constructor(private UserService) {
+        constructor(private $rootScope, private $state, private UserService) {
         }
 
 
         openLoginModal() {
             this.UserService.openLoginModal();
         }
+
+
+        showMyProfile = () => {
+            this.$state.go('tab.profile', {
+                userId: this.$rootScope.userID
+            });
+        };
 
         static
             controllerId:string = "TabCtrl";
