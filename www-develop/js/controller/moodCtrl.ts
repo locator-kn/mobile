9f3bc3d9aeeb47396f2d8d4428c7c2db691ef568
T1m1 <Ruprecht.t@gmx.de>
2015-06-21 23:26:07 +0200

    hide loading

--- www-develop/js/controller/moodCtrl.ts
+++ www-develop/js/controller/moodCtrl.ts
@@ -4,9 +4,10 @@ module Controller {
         moods:any = [];
         selected:any = [];
 
-        constructor(private DataService, private SearchService) {
+        constructor(private DataService, private SearchService, private $ionicLoading) {
             // get all moods from data service
             this.DataService.getAvailableMoods().then((result) => {
+                this.$ionicLoading.hide();
                 this.moods = result.data;
                 // old selected moods - needed because of state change
                 this.selected = this.SearchService.getMoods();
