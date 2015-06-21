07d6d6011a6445fe3e954c3fb8a6fb063cda1e85
T1m1 <Ruprecht.t@gmx.de>
2015-06-21 20:49:02 +0200

    refactor moods

--- www-develop/js/controller/moodCtrl.ts
+++ www-develop/js/controller/moodCtrl.ts
@@ -5,12 +5,13 @@ module Controller {
         selected:any = [];
 
         constructor(private DataService, private SearchService) {
+            // get all moods from data service
             this.DataService.getAvailableMoods().then((result) => {
                 this.moods = result.data;
-
+                // old selected moods - needed because of state change
                 this.selected = this.SearchService.getMoods();
 
-                // TODO: search alternative - only a hack
+                // TODO: search alternative for initialisation of old selections
                 for (var mood in this.moods) {
                     for (var selected in this.selected) {
                         if (this.moods[mood].query_name == this.selected[selected].query_name) {
@@ -30,15 +31,14 @@ module Controller {
                 this.selected.push(item);
                 // if we have gone over maxItems:
                 if (this.selected.length > 3) {
-                    //remove oldest item
-                    this.selected[0].checked = false;
-                    this.select(this.selected[0].query_name, false);
-                    this.selected.splice(0, 1);
+                    this.select(this.selected[3].query_name, false);
+                    this.selected.splice(3,1);
                 }
             } else {
                 // remove item
                 for (var i = 0; i < this.selected.length; i++) {
-                    if (this.selected[i] === item) {
+                    // diff by query name because if checks hashKey
+                    if (this.selected[i].query_name === item.query_name) {
                         this.selected.splice(i, 1);
                     }
                 }
