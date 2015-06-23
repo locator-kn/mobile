4dc6e82a9d6dcd935d6d98b3cee059c2195db7fc
T1m1 <Ruprecht.t@gmx.de>
2015-06-23 18:23:24 +0200

    add picture upload service

--- www-develop/js/app.ts
+++ www-develop/js/app.ts
@@ -22,6 +22,7 @@
 /// <reference path="./service/cameraService.ts" />
 /// <reference path="./service/geolocationService.ts" />
 /// <reference path="./service/tripService.ts" />
+/// <reference path="./service/pictureUploadService.ts" />
 
 // Ionic Starter App
 
@@ -79,6 +80,7 @@ angular.module('starter', deps)
     .service(Service.GeolocationService.serviceId, Service.GeolocationService)
     .service(Service.UserService.serviceId, Service.UserService)
     .service(Service.TripService.serviceId, Service.TripService)
+    .service(Service.PictureUploadService.serviceId, Service.PictureUploadService)
 
     .config(function (CacheFactoryProvider) {
         angular.extend(CacheFactoryProvider.defaults, {maxAge: 15 * 60 * 1000});
