c6c1c76a3192ec7cde9b1a88c1b11aafefdd534d
T1m1 <Ruprecht.t@gmx.de>
2015-06-25 20:05:50 +0200

    style last message

--- www-develop/js/controller/messengerCtrl.ts
+++ www-develop/js/controller/messengerCtrl.ts
@@ -16,6 +16,8 @@ module Controller {
                 .then(result => {
                     this.conversations = result.data;
                     this.conversations.forEach(element => {
+                        debugger;
+                        element.lastMessage = moment(new Date(element.modified_date)).startOf('hour').fromNow();
                         this.conversationsHash[element._id] = element;
                         this.UserService.getUser(element['opponent'])
                             .then(result => {
