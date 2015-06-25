95087d45b378945f2d9c5832f343f3e70f200232
T1m1 <Ruprecht.t@gmx.de>
2015-06-25 19:11:51 +0200

    impl functino to get conversations

--- www-develop/js/controller/messengerCtrl.ts
+++ www-develop/js/controller/messengerCtrl.ts
@@ -1,7 +1,28 @@
 module Controller {
     export class MessengerCtrl {
 
-        constructor(private $scope) {
+        conversations;
+        conversationsHash = {};
+
+
+        constructor(private MessengerService, private UserService, private $state, private basePath) {
+            this.getConversations();
+
+        }
+
+        // conversationlist
+        getConversations() {
+            this.MessengerService.getConversations()
+                .then(result => {
+                    this.conversations = result.data;
+                    this.conversations.forEach(element => {
+                        this.conversationsHash[element._id] = element;
+                        this.UserService.getUser(element['opponent'])
+                            .then(result => {
+                                element['opponent'] = result.data;
+                            });
+                    });
+                });
         }
 
         static controllerId:string = "MessengerCtrl";
