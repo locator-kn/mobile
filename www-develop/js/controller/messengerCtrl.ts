c59c25fc9db9df4e02aba370d409b21fb3b2a793
T1m1 <Ruprecht.t@gmx.de>
2015-06-26 20:50:39 +0200

    show minutes instead of hours

--- www-develop/js/controller/messengerCtrl.ts
+++ www-develop/js/controller/messengerCtrl.ts
@@ -16,7 +16,7 @@ module Controller {
                 .then(result => {
                     this.conversations = result.data;
                     this.conversations.forEach(element => {
-                        element.lastMessage = moment(new Date(element.modified_date)).startOf('hour').fromNow();
+                        element.lastMessage = moment(new Date(element.modified_date)).startOf('minutes').fromNow();
                         this.conversationsHash[element._id] = element;
                         this.UserService.getUser(element['opponent'])
                             .then(result => {
