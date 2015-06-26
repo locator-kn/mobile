4ed0586f951344e0127a5934669ee3940c3fe27c
T1m1 <Ruprecht.t@gmx.de>
2015-06-26 22:07:36 +0200

    improve screen heigth

--- www-develop/js/controller/welcomeCtrl.ts
+++ www-develop/js/controller/welcomeCtrl.ts
@@ -1,17 +1,10 @@
 module Controller {
     export class WelcomeCtrl {
 
-        private screenHeight;
-
         constructor(private $scope, private $element, $window) {
             // add logo to navbar
             $scope.navTitle = '<img src="./images/locator-logo.png" class="logo"/>';
-
-            this.screenHeight = window.innerHeight;
         }
-
-
-
         static controllerId:string = "WelcomeCtrl";
     }
 }
