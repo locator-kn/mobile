dbac94ef99a7a283f03f58713a44e603586630d1
T1m1 <Ruprecht.t@gmx.de>
2015-06-27 22:41:53 +0200

    refactor mood controller

--- www-develop/js/controller/moodCtrl.ts
+++ www-develop/js/controller/moodCtrl.ts
@@ -9,47 +9,11 @@ module Controller {
             this.DataService.getAvailableMoods().then((result) => {
                 this.$ionicLoading.hide();
                 this.moods = result.data;
-                // old selected moods - needed because of state change
-                this.selected = this.SearchService.getMoods();
-
-                // initialisation of old selections
-                for (var mood in this.moods) {
-                    for (var selected in this.selected) {
-                        if (this.moods[mood].query_name == this.selected[selected].query_name) {
-                            this.moods[mood].checked = true;
-                        }
-                    }
-                }
             });
         }
 
-        sync(bool, item) {
-            if (bool) {
-                // add item
-                this.selected.push(item);
-                // if we have gone over maxItems:
-                if (this.selected.length > 3) {
-                    this.select(this.selected[3].query_name, false);
-                    this.selected.splice(3, 1);
-                }
-            } else {
-                // remove item
-                for (var i = 0; i < this.selected.length; i++) {
-                    // diff by query name because if checks hashKey
-                    if (this.selected[i].query_name === item.query_name) {
-                        this.selected.splice(i, 1);
-                    }
-                }
-            }
-            this.SearchService.setMoods(this.selected);
-        }
-
-        select(query_name, value) {
-            for (var mood in this.moods) {
-                if (this.moods[mood].query_name == query_name) {
-                    this.moods[mood].checked = value;
-                }
-            }
+        setMood(mood) {
+            this.SearchService.setMood(mood);
         }
 
         static controllerId:string = "MoodCtrl";
