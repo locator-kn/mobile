cd3458354d11b3d148529e90a1681d8df11fff61
T1m1 <Ruprecht.t@gmx.de>
2015-06-28 22:51:20 +0200

    remove stellar

--- www-develop/js/app.ts
+++ www-develop/js/app.ts
@@ -118,24 +118,6 @@ angular.module('starter', deps)
         };
     })
 
-    .directive('stellax', function ($timeout) {
-        return {
-            restrict: 'A',
-            link: function ($scope, $element, $attr) {
-
-                $timeout(function () {
-                    (<any> $('.scroll')).stellar({
-                        scrollProperty: 'transform',
-                        positionProperty: 'transform',
-                        horizontalScrolling: false
-                        //,verticalOffset: -150
-                    });
-                });
-
-            }
-        };
-    })
-
     .directive('ngEnter', function () {
         return function (scope, element, attrs) {
             element.bind("keydown keypress", function (event) {
