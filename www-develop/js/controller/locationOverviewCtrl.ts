b4b673a2d0f469f41579def8f892214e7794f47d
T1m1 <Ruprecht.t@gmx.de>
2015-06-30 19:35:20 +0200

    get locations by user

--- www-develop/js/controller/locationOverviewCtrl.ts
+++ www-develop/js/controller/locationOverviewCtrl.ts
@@ -2,7 +2,32 @@ module Controller {
     export class LocationOverviewCtrl {
         results:any;
 
-        constructor() {
+        tripType = 'tab.search-result-locations';
+        profileType = 'tab.profile-locations';
+
+        state:string;
+        locationSourceId:string;
+
+        // ++++++++ TODO +++++++++++++++++
+        // - location by user x
+        // - location by my
+        // - location by city
+        // - location by trip
+        // +++++++++++++++++++++++++++++++
+
+        constructor(private $state, private $stateParams, private locationService) {
+            this.state = this.$state.current.name;
+
+            this.locationSourceId = $stateParams.locationSourceId;
+
+            if(this.state === this.tripType) {
+                // TODO call route to get all locations from trip
+            } else if (this.state === this.profileType) {
+                this.locationService.getLocationsByUser(this.locationSourceId).then((result) => {
+                    this.results = result;
+                })
+            }
+
 
         }
 
