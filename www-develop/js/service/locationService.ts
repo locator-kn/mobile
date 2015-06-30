fce2994432bce7a1e570b565cc83122d79875544
T1m1 <Ruprecht.t@gmx.de>
2015-07-01 00:38:42 +0200

    implement function to get all location by tripId

--- www-develop/js/service/locationService.ts
+++ www-develop/js/service/locationService.ts
@@ -1,12 +1,35 @@
 module Service {
     export class LocationService {
 
-        constructor(private $http, private basePath) {
+        constructor(private $http, private basePath, private SearchService, private $q) {
         }
 
 
         getLocationsByUser(userId) {
             return this.$http.get(this.basePath + '/users/' + userId + '/locations');
+        }
+
+        getLocationsByTripId(tripId) {
+            var q = this.$q.defer();
+
+            this.SearchService.getTripById(tripId).then((result) => {
+                var locations = [];
+                for (var location in result.data.locations) {
+                    var promise = this.$http.get(this.basePath + '/locations/' + location);
+                    locations.push(promise);
+                }
+                this.$q.all(locations).then((data) => {
+                    var result = {
+                        data: []
+                    };
+                    var index;
+                    for (index = 0; index < data.length; ++index) {
+                        result.data.push(data[index].data);
+                    }
+                    q.resolve(result)
+                });
+            });
+            return q.promise;
         }
 
         static serviceId:string = "LocationService";
