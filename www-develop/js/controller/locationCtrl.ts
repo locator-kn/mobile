c3500ed750b2d3b1bafa22f07332ce62f264df44
T1m1 <Ruprecht.t@gmx.de>
2015-07-01 22:45:35 +0200

    add default locations

--- www-develop/js/controller/locationCtrl.ts
+++ www-develop/js/controller/locationCtrl.ts
@@ -5,11 +5,45 @@ module Controller {
 
         user:any = {};
 
-        constructor(private UserService, private $stateParams, private LocationService, private $ionicLoading, private webPath) {
+        map:any = {};
+        mapMarkerSet:boolean;
+
+        constructor(private UserService, private $scope, private $stateParams, private LocationService, private $ionicLoading, private webPath) {
             this.locationId = $stateParams.locationId;
+
+            this.map = {center: {latitude: 45, longitude: -73}, zoom: 8};
 
             this.LocationService.getLocationById(this.locationId).then((result)=> {
                 this.result = result.data;
+
+                var lat = result.data.geotag.lat;
+                var long = result.data.geotag.long;
+                this.map = {
+                    center: {
+                        // kn fh
+                        latitude: lat,
+                        longitude: long
+                    },
+                    zoom: 15,
+                    clickedMarker: {
+                        latitude: lat,
+                        longitude: long,
+                        id: 2,
+                        options: {
+                            labelClass: 'marker-labels',
+                            labelAnchor: '50 0'
+                        }
+
+                    },
+                    scrollwheel	: false,
+                    navigationControl	: false,
+                    mapTypeControl	: false,
+                    scaleControl	: false,
+                    draggable	: false
+                };
+
+
+                this.mapMarkerSet = true;
 
                 this.UserService.getUser(this.result.userid)
                     .then(result => {
