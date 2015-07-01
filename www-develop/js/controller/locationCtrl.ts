07eb71cd363cda1769ed591970dceddaa8bbbe0a
T1m1 <Ruprecht.t@gmx.de>
2015-07-01 11:19:45 +0200

    call function to get location

--- www-develop/js/controller/locationCtrl.ts
+++ www-develop/js/controller/locationCtrl.ts
@@ -5,6 +5,11 @@ module Controller {
 
         constructor(private $state, private $stateParams, private LocationService, private $ionicLoading, private webPath) {
             this.locationId = $stateParams.locationId;
+
+            this.LocationService.getLocationById(this.locationId).then((result)=> {
+                this.results = result.data;
+            })
+
         }
 
         static controllerId:string = "LocationCtrl";
