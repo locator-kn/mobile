8d29726fc9375e1f868a1f78247cbe43c8a6d066
T1m1 <Ruprecht.t@gmx.de>
2015-07-01 13:01:41 +0200

    use backend function

--- www-develop/js/service/locationService.ts
+++ www-develop/js/service/locationService.ts
@@ -10,26 +10,7 @@ module Service {
         }
 
         getLocationsByTripId(tripId) {
-            var q = this.$q.defer();
-
-            this.SearchService.getTripById(tripId).then((result) => {
-                var locations = [];
-                for (var location in result.data.locations) {
-                    var promise = this.$http.get(this.basePath + '/locations/' + location);
-                    locations.push(promise);
-                }
-                this.$q.all(locations).then((data) => {
-                    var result = {
-                        data: []
-                    };
-                    var index;
-                    for (index = 0; index < data.length; ++index) {
-                        result.data.push(data[index].data);
-                    }
-                    q.resolve(result)
-                });
-            });
-            return q.promise;
+            return this.$http.get(this.basePath + '/trips/' + tripId + '/locations');
         }
 
         getLocationById(locationId) {
