39eb6d5d7dcbd40d241bb1a37da0d0736fca6c71
T1m1 <Ruprecht.t@gmx.de>
2015-07-01 23:28:11 +0200

    revert map implementation

--- www-develop/js/controller/locationCtrl.ts
+++ www-develop/js/controller/locationCtrl.ts
@@ -5,45 +5,11 @@ module Controller {
 
         user:any = {};
 
-        map:any = {};
-        mapMarkerSet:boolean;
-
         constructor(private UserService, private $scope, private $stateParams, private LocationService, private $ionicLoading, private webPath) {
             this.locationId = $stateParams.locationId;
 
-            this.map = {center: {latitude: 45, longitude: -73}, zoom: 8};
-
             this.LocationService.getLocationById(this.locationId).then((result)=> {
                 this.result = result.data;
-
-                var lat = result.data.geotag.lat;
-                var long = result.data.geotag.long;
-                this.map = {
-                    center: {
-                        // kn fh
-                        latitude: lat,
-                        longitude: long
-                    },
-                    zoom: 15,
-                    clickedMarker: {
-                        latitude: lat,
-                        longitude: long,
-                        id: 2,
-                        options: {
-                            labelClass: 'marker-labels',
-                            labelAnchor: '50 0'
-                        }
-
-                    },
-                    scrollwheel	: false,
-                    navigationControl	: false,
-                    mapTypeControl	: false,
-                    scaleControl	: false,
-                    draggable	: false
-                };
-
-
-                this.mapMarkerSet = true;
 
                 this.UserService.getUser(this.result.userid)
                     .then(result => {
@@ -51,7 +17,6 @@ module Controller {
                         this.$ionicLoading.hide();
                     });
             })
-
         }
 
         static controllerId:string = "LocationCtrl";
