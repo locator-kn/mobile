2a80824bd6576c219a214d8e4e2d3856554802ea
T1m1 <Ruprecht.t@gmx.de>
2015-07-02 02:00:38 +0200

    impl function to save picture in location

--- www-develop/js/controller/insertLocationCtrl.ts
+++ www-develop/js/controller/insertLocationCtrl.ts
@@ -2,13 +2,71 @@ module Controller {
     export class InsertLocationCtrl {
         imageURI;
 
+        result:any = {};
+
+        //trip
+        documentId:string;
+        locationTitle:string;
+        revision:string;
+
         lat:string;
         long:string;
+
+        me:any = {};
 
         pictureWidth:number = 256;
         pictureHeight:number = 150;
 
-        constructor(private CameraService, private GeolocationService) {
+
+        // TODO: refactor
+        uploadIsDone:boolean;
+        isUploading:boolean;
+        documentWasCreated:boolean;
+
+        constructor(private CameraService, private basePath, private GeolocationService, private UserService,
+                    private PictureUploadService) {
+
+            this.UserService.getMe().then(user => {
+                this.me = user.data;
+            });
+        }
+
+        uploadImage(image) {
+            this.uploadIsDone = false;
+
+            this.isUploading = true;
+            var file = image.src;
+            var formData = {
+                width: Math.round(image.width),
+                height: Math.round(image.height),
+                xCoord: Math.round(image.x),
+                yCoord: Math.round(image.y),
+                locationTitle: this.locationTitle.toLowerCase() || 'supertrip',
+                _id: '',
+                _rev: ''
+            };
+
+            if (this.documentWasCreated) {
+                formData._id = this.documentId;
+                formData._rev = this.revision;
+            }
+            this.PictureUploadService.uploadImage(file, this.basePath + '/users/my/locations/picture', formData)
+                .error(() => {
+                    this.isUploading = false;
+                })
+                .progress(evt => {
+                    // TODO: impl processing
+                }).success((data, status, headers, config) => {
+                    console.log('file', config.file.name, 'uploaded. Response:', data);
+
+                    //this.showNewImage(data);
+                    this.documentId = data.id;
+                    this.revision = data.rev;
+                    this.uploadIsDone = true;
+                    this.isUploading = false;
+                });
+
+            //this.InsertTripService.uploadImage(formData);
         }
 
         // sample
@@ -19,13 +77,17 @@ module Controller {
             })
         };
 
-        showPictureActions= () => {
+        showPictureActions = () => {
             var opt = {
                 width: this.pictureWidth,
                 height: this.pictureHeight
             };
             this.CameraService.selectPicture(opt).then((data) => {
+
+                // TODO: check image size
+
                 this.imageURI = data;
+
             });
         };
 
