de859eb5338667aaca59d5475556d7198f5b892c
T1m1 <Ruprecht.t@gmx.de>
2015-07-02 15:58:29 +0200

    remove function

--- www-develop/js/service/geolocationService.ts
+++ www-develop/js/service/geolocationService.ts
@@ -11,35 +11,12 @@ module Service {
             var posOptions = {maximumAge: 3000, timeout: 5000, enableHighAccuracy: true};
             this.$ionicLoading.show({template: '<ion-spinner icon="spiral"></ion-spinner>'});
             navigator.geolocation.getCurrentPosition((result) => {
-                // Do any magic we need
+                this.$ionicLoading.hide();
                 q.resolve(result);
             }, (err) => {
+                this.$ionicLoading.hide();
                 q.reject(err)
             }, posOptions);
-
-            return q.promise;
-        }
-
-        getGeoLocation() {
-            var q = this.$q.defer();
-// Show the action sheet
-            var hideSheet = this.$ionicActionSheet.show({
-                buttons: [
-                    {text: 'Finde mich'},
-                    {text: 'Google Map'}
-                ],
-                titleText: 'Geolocation bestimmen',
-                cancelText: 'Abbrechen',
-                cancel: ()=> {
-                    hideSheet();
-                },
-                buttonClicked: (index) => {
-                    if (index === 0) {
-                    } else {
-                            hideSheet();
-                    }
-                }
-            });
 
             return q.promise;
         }
