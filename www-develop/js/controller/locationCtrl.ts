5e6dc8b9b8369d6599cbf081ef388c676b06474b
T1m1 <Ruprecht.t@gmx.de>
2015-07-03 22:06:28 +0200

    format date

--- www-develop/js/controller/locationCtrl.ts
+++ www-develop/js/controller/locationCtrl.ts
@@ -10,6 +10,7 @@ module Controller {
 
             this.LocationService.getLocationById(this.locationId).then((result)=> {
                 this.result = result.data;
+                this.result.create_date = moment(new Date(this.result.create_date)).format('L');
 
                 this.UserService.getUser(this.result.userid)
                     .then(result => {
