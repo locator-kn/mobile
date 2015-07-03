fbae3fe038015c541838a28394c12fcc07627eca
T1m1 <Ruprecht.t@gmx.de>
2015-07-03 17:27:10 +0200

    show alert instead of info

--- www-develop/js/service/geolocationService.ts
+++ www-develop/js/service/geolocationService.ts
@@ -8,14 +8,14 @@ module Service {
 
         getCurrentLocation() {
             var q = this.$q.defer();
-            var posOptions = {maximumAge: 4000, timeout: 6000, enableHighAccuracy: true};
+            var posOptions = {maximumAge: 3000, timeout: 5000, enableHighAccuracy: true};
             this.$ionicLoading.show({template: '<ion-spinner icon="spiral"></ion-spinner>'});
             navigator.geolocation.getCurrentPosition((result) => {
                 this.$ionicLoading.hide();
                 q.resolve(result);
             }, (err) => {
                 this.$ionicLoading.hide();
-                this.$ionicPopup.show({title: 'Überprüfe deine GPS Verbindung und versuche es erneut'});
+                this.$ionicPopup.alert({title: 'Überprüfe deine GPS Verbindung und versuche es erneut'});
                 q.reject(err)
             }, posOptions);
 
