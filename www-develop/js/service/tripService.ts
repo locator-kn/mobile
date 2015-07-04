6dbd3f4ea43562b9bbe02370bece383593ab9b1a
T1m1 <Ruprecht.t@gmx.de>
2015-07-05 01:33:49 +0200

    impl. equipment to insert trip

--- www-develop/js/service/tripService.ts
+++ www-develop/js/service/tripService.ts
@@ -2,7 +2,8 @@ module Service {
     export class TripService {
 
         // create trip
-        city:any={};
+        city:any = {};
+        accommodationEquipment:any = {};
 
         constructor(private $http, private basePath, private $ionicLoading, private $rootScope) {
         }
@@ -21,6 +22,14 @@ module Service {
             return this.city;
         }
 
+        setAccommodationEquipment(equipment) {
+            this.accommodationEquipment = equipment;
+            this.$rootScope.$emit('newInsertTripAccommodationEquipment');
+        }
+
+        getAccommodationEquipment() {
+           return this.accommodationEquipment;
+        }
         static serviceId:string = "TripService";
     }
 }
