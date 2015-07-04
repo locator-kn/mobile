3f0980950070fd0817a0697e134c8bcf994ab70c
T1m1 <Ruprecht.t@gmx.de>
2015-07-05 00:30:33 +0200

    implement persons for insert trip

--- www-develop/js/controller/insertTripCtrl.ts
+++ www-develop/js/controller/insertTripCtrl.ts
@@ -3,12 +3,14 @@ module Controller {
 
         // trip
         city:any = {};
-        selectedDays:number;
         start_date;
         end_date;
+        selectedDays:number;
+        selectedPersons:number;
 
         // static data
         availableDays:any = [];
+        availablePersons:any = [];
 
         constructor(private $rootScope, private TripService, private DataService) {
             $rootScope.$on('newInsertTripCity', () => {
@@ -18,6 +20,11 @@ module Controller {
             this.DataService.getAvailableDays().then((result)=> {
                 this.availableDays = result.data;
             });
+
+            this.DataService.getAvailablePersons().then((result) => {
+                this.availablePersons = result.data;
+            });
+
         }
 
         static controllerId:string = "InsertTripCtrl";
