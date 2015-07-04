b517f0d90a79fc30d5ed93ad2231c9362515d876
T1m1 <Ruprecht.t@gmx.de>
2015-07-05 01:33:32 +0200

    impl. equipment to insert trip

--- www-develop/js/controller/insertTripCtrl.ts
+++ www-develop/js/controller/insertTripCtrl.ts
@@ -7,15 +7,23 @@ module Controller {
         end_date;
         selectedDays:number;
         selectedPersons:number;
+        accommodation:boolean;
+        selectedAccommodationEquipment:any = [];
 
         // static data
         availableDays:any = [];
         availablePersons:any = [];
+        availableAccommodationEquipment:any = [];
 
         constructor(private $rootScope, private TripService, private DataService) {
             $rootScope.$on('newInsertTripCity', () => {
                 this.city = this.TripService.getCity();
             });
+
+            $rootScope.$on('newInsertTripAccommodationEquipment', () => {
+                this.selectedAccommodationEquipment = this.TripService.getAccommodationEquipment();
+            });
+
 
             this.DataService.getAvailableDays().then((result)=> {
                 this.availableDays = result.data;
@@ -25,6 +33,9 @@ module Controller {
                 this.availablePersons = result.data;
             });
 
+            this.DataService.getAvailableAccommodationEquipment().then((result) => {
+                this.availableAccommodationEquipment = result.data;
+            });
         }
 
         static controllerId:string = "InsertTripCtrl";
