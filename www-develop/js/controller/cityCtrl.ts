ce3b396ccef0ac6cf3020e200c64ef727fad1bbc
T1m1 <Ruprecht.t@gmx.de>
2015-07-05 00:22:06 +0200

    extend city controller to use it for search and insert

--- www-develop/js/controller/cityCtrl.ts
+++ www-develop/js/controller/cityCtrl.ts
@@ -2,8 +2,15 @@ module Controller {
     export class CityCtrl {
 
         cities:any = {};
+        state;
 
-        constructor(private DataService, private SearchService, private $ionicLoading) {
+        offerState = 'tab.offer';
+        citySearchState = 'tab.search-city';
+        searchState = 'tab.search';
+
+
+        constructor(private DataService, private SearchService, private $ionicLoading, private $state, private TripService) {
+            this.state = this.$state.current.name;
             this.DataService.getAvailableCities().then((result) => {
                 this.cities = result.data;
                 this.$ionicLoading.hide();
@@ -11,7 +18,13 @@ module Controller {
         }
 
         setCity(city) {
-            this.SearchService.setCity(city);
+            if(this.state === this.citySearchState) {
+                this.SearchService.setCity(city);
+                this.$state.go(this.searchState);
+            } else {
+                this.TripService.setCity(city);
+                this.$state.go(this.offerState);
+            }
         }
 
         static
