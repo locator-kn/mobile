c9d0ebe1f039fa24a663d18c9d2ffb56f06d5ac7
T1m1 <Ruprecht.t@gmx.de>
2015-07-05 01:32:57 +0200

    add equipment to data service

--- www-develop/js/service/dataService.ts
+++ www-develop/js/service/dataService.ts
@@ -5,12 +5,14 @@ module Service {
         dataDaysCache;
         dataPersonsCache;
         dataMoodsCache;
+        accommodationEquipment;
 
         constructor(private $http, private basePath, private CacheFactory, private $q, private $ionicLoading) {
             this.dataCitiesCache = CacheFactory.createCache('dataCities');
             this.dataDaysCache = CacheFactory.createCache('dataDays');
             this.dataPersonsCache = CacheFactory.createCache('dataPersons');
             this.dataMoodsCache = CacheFactory.createCache('dataMoods');
+            this.accommodationEquipment = CacheFactory.createCache('accommodationEquipment');
         }
 
         getAvailableCities() {
@@ -45,6 +47,66 @@ module Service {
                                 {value: 3, title: "3"},
                                 {value: 4, title: "4"},
                                 {value: 5, title: "4+"}
+                            ]
+                        });
+                }, 0);
+            }, {cache: this.dataPersonsCache});
+        }
+
+        getAvailableAccommodationEquipment() {
+            return this.$q(function (resolve) {
+                setTimeout(function () {
+                    resolve(
+                        {
+                            data: [
+                                {
+                                    query_name: 'wifi',
+                                    title: "WLAN",
+                                    icon: "wifi.png",
+                                    icon_grey: "wifi_grey.png"
+                                },
+                                {
+                                    query_name: 'tv',
+                                    title: "TV",
+                                    icon: "tv.png",
+                                    icon_grey: "tv_grey.png"
+                                },
+                                {
+                                    query_name: 'shower',
+                                    title: "Dusche/Bad",
+                                    icon: "shower.png",
+                                    icon_grey: "shower_grey.png"
+                                },
+                                {
+                                    query_name: 'meal',
+                                    title: "Küche",
+                                    icon: "kitchen.png",
+                                    icon_grey: "kitchen_grey.png"
+                                },
+                                {
+                                    query_name: 'breakfast',
+                                    title: "Frühstück",
+                                    icon: "breakfast.png",
+                                    icon_grey: "breakfast_grey.png"
+                                },
+                                {
+                                    query_name: 'smoker',
+                                    title: "Raucher",
+                                    icon: "smoking.png",
+                                    icon_grey: "smoking_grey.png"
+                                },
+                                {
+                                    query_name: 'parking',
+                                    title: "Parkplätze",
+                                    icon: "parking.png",
+                                    icon_grey: "parking_grey.png"
+                                },
+                                {
+                                    query_name: 'handicapped',
+                                    title: "Behindertengerecht",
+                                    icon: "wheelchair.png",
+                                    icon_grey: "wheelchair_grey.png"
+                                }
                             ]
                         });
                 }, 0);
