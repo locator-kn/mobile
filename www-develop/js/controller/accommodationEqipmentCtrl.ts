72bbdf571e2ed8e0e6d3fcad1f09c929834bc8a3
T1m1 <Ruprecht.t@gmx.de>
2015-07-05 02:32:33 +0200

    implement multiple choose

--- www-develop/js/controller/accommodationEqipmentCtrl.ts
+++ www-develop/js/controller/accommodationEqipmentCtrl.ts
@@ -9,11 +9,40 @@ module Controller {
             this.DataService.getAvailableAccommodationEquipment().then((result) => {
                 this.$ionicLoading.hide();
                 this.availableAccommodationEquipment = result.data;
+
+                this.selectedAccommodationEquipment = this.TripService.getAccommodationEquipment();
+
+                debugger;
+
+                // initialisation of old selections
+                for (var equipment in this.availableAccommodationEquipment) {
+                    for (var selected in this.selectedAccommodationEquipment) {
+                        if (this.availableAccommodationEquipment[equipment].query_name == this.selectedAccommodationEquipment[selected].query_name) {
+                            this.availableAccommodationEquipment[equipment].checked = true;
+                        }
+                    }
+                }
             });
         }
 
-        setAccommodationEquipment(equipment) {
-            this.TripService.setAccommodationEquipment(equipment);
+        sync = (bool, item) =>  {
+            if (bool) {
+                // add item
+                this.selectedAccommodationEquipment.push(item);
+            } else {
+                // remove item
+                for (var i = 0; i < this.selectedAccommodationEquipment.length; i++) {
+                    // diff by query name because if checks hashKey
+                    if (this.selectedAccommodationEquipment[i].query_name === item.query_name) {
+                        this.selectedAccommodationEquipment.splice(i, 1);
+                    }
+                }
+            }
+            this.TripService.setAccommodationEquipment (this.selectedAccommodationEquipment);
+        };
+
+        setAccommodationEquipment() {
+            this.TripService.setAccommodationEquipment(this.selectedAccommodationEquipment);
             this.$state.go('tab.offer');
         }
 
