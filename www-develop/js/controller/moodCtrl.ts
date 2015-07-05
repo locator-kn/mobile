56b5bb5bf8382dbe8e488c189f2718c50a0863ac
T1m1 <Ruprecht.t@gmx.de>
2015-07-05 02:43:33 +0200

    extend mood controller to use it for search and insert

--- www-develop/js/controller/moodCtrl.ts
+++ www-develop/js/controller/moodCtrl.ts
@@ -4,7 +4,15 @@ module Controller {
         moods:any = [];
         selected:any = [];
 
-        constructor(private DataService, private SearchService, private $ionicLoading) {
+        offerState = 'tab.offer';
+        moodSearchState = 'tab.search-mood';
+        searchState = 'tab.search';
+
+        state:string;
+
+        constructor(private DataService, private SearchService, private $ionicLoading, private $state, private TripService) {
+            this.state = this.$state.current.name;
+
             // get all moods from data service
             this.DataService.getAvailableMoods().then((result) => {
                 this.$ionicLoading.hide();
@@ -13,7 +21,14 @@ module Controller {
         }
 
         setMood(mood) {
-            this.SearchService.setMood(mood);
+            debugger;
+            if(this.state === this.moodSearchState) {
+                this.SearchService.setMood(mood);
+                this.$state.go(this.searchState);
+            } else {
+                this.TripService.setMood(mood);
+                this.$state.go(this.offerState);
+            }
         }
 
         static controllerId:string = "MoodCtrl";
