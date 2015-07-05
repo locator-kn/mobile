8c07d425b04d7e52e306e66e06dd15a151db6161
T1m1 <Ruprecht.t@gmx.de>
2015-07-05 23:33:07 +0200

    implememnt trip preview controller

--- www-develop/js/controller/tripPreviewCtrl.ts
+++ www-develop/js/controller/tripPreviewCtrl.ts
@@ -2,34 +2,31 @@ module Controller {
     export class TripPreviewCtrl {
 
         trip:any = {};
-        tripId:string;
-
         user:any = {};
-
         moods:any = [];
 
-        constructor(private $scope, private $element, private $stateParams, private SearchService, private DataService,
-                    private $ionicSlideBoxDelegate, private UserService, private $ionicLoading, private webPath) {
-            // get trip by id from state param
-            SearchService.getTripById(this.$stateParams.tripId).then((result) => {
-                this.trip = result.data;
-                if (this.trip.start_date && this.trip.end_date) {
-                    // date format
-                    this.trip.start_date = moment(new Date(this.trip.start_date)).format('L');
-                    this.trip.end_date = moment(new Date(this.trip.end_date)).format('L');
-                }
-                // important for ion-slide!
-                this.$ionicSlideBoxDelegate.update();
-                // workaround, because title do not update in tripDetail.html
-                $scope.navTitle = this.trip.title;
+        constructor(private TripService, private $ionicSlideBoxDelegate, private $scope, private UserService,
+                    private DataService, private $ionicLoading) {
+            this.trip = TripService.getPreTrip();
+            this.trip.locations = TripService.getLocations();
 
-                this.UserService.getUser(this.trip.userid)
-                    .then(result => {
-                        this.user = result.data;
-                        this.$ionicLoading.hide();
-                    });
-            });
+            if (this.trip.start_date && this.trip.end_date) {
+                // date format
+                this.trip.start_date = moment(new Date(this.trip.start_date)).format('L');
+                this.trip.end_date = moment(new Date(this.trip.end_date)).format('L');
+            }
 
+            // workaround, because title do not update in tripDetail.html
+            $scope.navTitle = this.trip.title;
+
+            // important for ion-slide!
+            this.$ionicSlideBoxDelegate.update();
+
+            this.UserService.getMe()
+                .then(result => {
+                    this.user = result.data;
+                    this.$ionicLoading.hide();
+                });
 
             this.DataService.getAvailableMoods().then((result) => {
                 this.moods = result.data;
