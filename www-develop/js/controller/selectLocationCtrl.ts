08e7e4a4ceb14a1839a5457cd18bba0abc20dbdd
T1m1 <Ruprecht.t@gmx.de>
2015-07-05 11:22:27 +0200

    get all needed locations

--- www-develop/js/controller/selectLocationCtrl.ts
+++ www-develop/js/controller/selectLocationCtrl.ts
@@ -1,8 +1,22 @@
 module Controller {
     export class SelectLocationCtrl {
-        result:any = {};
+        myLocationsByCity:any = {};
+        locationsByCity:any = {};
 
-        constructor() {
+        cityId:string;
+
+        constructor(private LocationService, private $stateParams) {
+            this.cityId = $stateParams.cityId;
+
+            console.log(this.cityId);
+
+            this.LocationService.getMyLocationsByCity(this.cityId).then((result) => {
+                this.myLocationsByCity = result.data;
+            });
+
+            this.LocationService.getLocationsByCity(this.cityId).then((result)=> {
+                this.locationsByCity = result.data
+            })
         }
 
         static controllerId:string = "SelectLocationCtrl";
