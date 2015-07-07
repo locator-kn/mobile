13bfd1393926ed46528d074474e4208d8afc3c4b
T1m1 <Ruprecht.t@gmx.de>
2015-07-07 22:56:58 +0200

    get first element of location and impl. function restart

--- www-develop/js/controller/optionCtrl.ts
+++ www-develop/js/controller/optionCtrl.ts
@@ -2,18 +2,22 @@ module Controller {
     export class OptionCtrl {
 
         tripId:string;
-        locations:any = {};
-        constructor(private webPath, private TripService, private $ionicScrollDelegate) {
+        location:any = {};
+        constructor(private webPath, private TripService, private $ionicScrollDelegate, private $window, private $state) {
             // TODO check if location state or trip state
             var result = TripService.getResultInfoObject();
             this.tripId = result.tripId;
-            this.locations = result.locations;
+
+            for (var first in result.locations) break;
+            this.location = result.locations[first];
 
             this.$ionicScrollDelegate.scrollTop(true);
 
-            //this.$window.location.reload(true);
+        }
 
-
+        restart() {
+            this.$state.go('tab.offer');
+            this.$window.location.reload(true);
         }
 
         static controllerId:string = "OptionCtrl";
