acfb5a3809d4c23fb637985e65b68c5cf333f542
T1m1 <Ruprecht.t@gmx.de>
2015-07-08 00:16:47 +0200

    create restart functions for different states

--- www-develop/js/controller/optionCtrl.ts
+++ www-develop/js/controller/optionCtrl.ts
@@ -6,10 +6,13 @@ module Controller {
         locateType = 'tab.locate-options';
         state;
 
+        isLocation:boolean;
+
         constructor(private webPath, private TripService, private GeolocationService, private $ionicScrollDelegate, private $window, private $state) {
             this.state = this.$state.current.name;
 
             if (this.state === this.locateType) {
+                this.isLocation = true;
                 this.result = GeolocationService.getResultInfoObject();
             } else {
                 this.result = TripService.getResultInfoObject();
@@ -19,8 +22,13 @@ module Controller {
 
         }
 
-        restart() {
+        restartTrip() {
             this.$state.go('tab.offer');
+            this.$window.location.reload(true);
+        }
+
+        restartLocation() {
+            this.$state.go('tab.locate');
             this.$window.location.reload(true);
         }
 
