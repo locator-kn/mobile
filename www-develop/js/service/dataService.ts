7d7d2582a25acb415ec4b53ea1044284c4a6e889
T1m1 <Ruprecht.t@gmx.de>
2015-07-09 20:12:07 +0200

    change path and key in dataservice for white icons

--- www-develop/js/service/dataService.ts
+++ www-develop/js/service/dataService.ts
@@ -122,73 +122,73 @@ module Service {
                                     query_name: "buddytrip",
                                     title: "buddytrip",
                                     icon: "buddytrip.png",
-                                    icon_grey: "buddytrip_grey.png",
+                                    icon_white: "buddytrip_weiss.png",
                                     description: ''
                                 }, {
                                     query_name: "girls_on_tour",
                                     title: "girls on tour",
                                     icon: "girls_on_tour.png",
-                                    icon_grey: "girls_on_tour_grey.png",
+                                    icon_white: "girls_on_tour_weiss.png",
                                     description: ""
                                 }, {
                                     query_name: "halligalli_drecksau",
                                     title: "halligalli drecksau",
                                     icon: "halligalli_drecksau.png",
-                                    icon_grey: "halligalli_drecksau_grey.png",
+                                    icon_white: "halligalli_drecksau_weiss.png",
                                     description: ""
                                 }, {
                                     query_name: "muskelkater",
                                     title: "muskelkater",
                                     icon: "muskelkater.png",
-                                    icon_grey: "muskelkater_grey.png",
+                                    icon_white: "muskelkater_weiss.png",
                                     description: ""
                                 }, {
                                     query_name: "sturm_der_liebe",
                                     title: "sturm der liebe",
                                     icon: "sturm_der_liebe.png",
-                                    icon_grey: "sturm_der_liebe_grey.png",
+                                    icon_white: "sturm_der_liebe_weiss.png",
                                     description: ""
                                 }, {
                                     query_name: "gruener_gehts_nicht",
                                     title: "gruener gehts nicht",
                                     icon: "gruener_gehts_nicht.png",
-                                    icon_grey: "gruener_gehts_nicht_grey.png",
+                                    icon_white: "gruener_gehts_nicht_weiss.png",
                                     description: ""
                                 }, {
                                     query_name: "haste_nicht_gesehen",
                                     title: "haste nicht gesehen",
                                     icon: "haste_nicht_gesehen.png",
-                                    icon_grey: "haste_nicht_gesehen_grey.png",
+                                    icon_white: "haste_nicht_gesehen_weiss.png",
                                     description: ""
                                 }, {
                                     query_name: "singles_unter_sich",
                                     title: "singles unter sich",
                                     icon: "singles_unter_sich.png",
-                                    icon_grey: "singles_unter_sich_grey.png",
+                                    icon_white: "singles_unter_sich_weiss.png",
                                     description: ""
                                 }, {
                                     query_name: "family_fun",
                                     title: "family fun",
                                     icon: "family_fun.png",
-                                    icon_grey: "family_fun_grey.png",
+                                    icon_white: "family_fun_weiss.png",
                                     description: ""
                                 }, {
                                     query_name: "kultur_und_sightseeing",
                                     title: "kultur und sightseeing",
                                     icon: "kultur_und_sightseeing.png",
-                                    icon_grey: "kultur_und_sightseeing_grey.png",
+                                    icon_white: "kultur_und_sightseeing_weiss.png",
                                     description: ""
                                 }, {
                                     query_name: 'leckermaeulchen',
                                     title: 'leckermaeulchen',
                                     icon: 'leckermaeulchen.png',
-                                    icon_grey: 'leckermaeulchen_grey.png',
+                                    icon_white: 'leckermaeulchen_weiss.png',
                                     description: ''
                                 }, {
                                     query_name: "entspannung_pur",
                                     title: "entspannnung pur",
                                     icon: "entspannung_pur.png",
-                                    icon_grey: "entspannung_pur_grey.png",
+                                    icon_white: "entspannung_pur_weiss.png",
                                     description: ""
                                 }
                             ]
