062a1387207801f74f1ecc759adceac8c26debab
T1m1 <Ruprecht.t@gmx.de>
2015-07-09 02:27:46 +0200

    implement functino to provide badges

--- www-develop/js/service/messengerService.ts
+++ www-develop/js/service/messengerService.ts
@@ -1,17 +1,17 @@
 module Service {
     export class MessengerService {
         messagesIdCache;
+        badgeBash:any = {};
 
-        constructor(private $http, private CacheFactory, private basePathRealtime, private UserService) {
+        constructor(private $http, private CacheFactory, private basePathRealtime, private UserService, private $rootScope) {
             this.messagesIdCache = this.CacheFactory.createCache('messagesId');
         }
-
 
         getConversations() {
             return this.$http.get(this.basePathRealtime + '/my/conversations');
         }
 
-        getConversationById(conversationId){
+        getConversationById(conversationId) {
             return this.$http.get(this.basePathRealtime + '/conversations/' + conversationId)
         }
 
@@ -42,6 +42,33 @@ module Service {
 
         clearMessageCacheById(messageId) {
             this.messagesIdCache.remove(this.basePathRealtime + '/messages/' + messageId);
+        }
+
+        setBadgeHash(badgeHash) {
+            this.badgeBash = badgeHash;
+            for (var element in this.badgeBash) {
+                if (!this.badgeBash[element]) {
+                    this.$rootScope.badge += 1;
+                }
+            }
+        }
+
+        badgeStatusOf(conversationId) {
+            return this.badgeBash[conversationId];
+        }
+
+        updateBadge(conversationId:string, newMessage:boolean) {
+            var status = this.badgeBash[conversationId];
+            if (status === false) {
+                if (newMessage === true) {
+                    this.$rootScope.badge -= 1;
+                }
+            } else {
+                if (newMessage === false) {
+                    this.$rootScope.badge += 1;
+                }
+            }
+            this.badgeBash[conversationId] = newMessage;
         }
 
         static serviceId:string = "MessengerService";
