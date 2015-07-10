18d5a904764a4669ea7b8bb64d293b5f5c6870ea
T1m1 <Ruprecht.t@gmx.de>
2015-07-10 17:24:55 +0200

    implement function for google map

--- www-develop/js/controller/googleMapCtrl.ts
+++ www-develop/js/controller/googleMapCtrl.ts
@@ -3,7 +3,14 @@ module Controller {
 
         map:any = {};
 
-        constructor() {
+        googleMap:boolean = false;
+        mapIsReady:boolean = false;
+        mapMarkerSet:boolean;
+
+        lat:string;
+        long:string;
+
+        constructor(private $ionicLoading, private GeolocationService, private $scope) {
             this.map = {
                 center: {
                     // kn fh
@@ -16,9 +23,68 @@ module Controller {
                     latitude: null,
                     longitude: null
                 },
-                //events: this.getEvents()
+                events: this.getEvents()
             };
         }
+
+        getEvents() {
+            return {
+                mousedown: (mapModel, eventName, originalEventArgs) => {
+                    this.clickMapEvent(mapModel, eventName, originalEventArgs);
+                },
+                tilesloaded: () => {
+                    this.mapIsReady = true;
+                    this.getCurrentPosition();
+                    this.$ionicLoading.hide();
+
+                }
+            }
+        }
+
+        clickMapEvent(mapModel, eventName, originalEventArgs) {
+            var e = originalEventArgs[0];
+            var lat = e.latLng.lat(),
+                lon = e.latLng.lng();
+            this.map.clickedMarker = {
+                id: 0,
+                options: {
+                    labelClass: "marker-labels",
+                    labelAnchor: "50 0"
+                },
+                latitude: lat,
+                longitude: lon
+            };
+            this.mapMarkerSet = true;
+            this.GeolocationService.setGeoPosition(this.map);
+        }
+
+        // sample
+        getCurrentPosition = () => {
+            if (this.mapIsReady) {
+                if (!this.googleMap) {
+                    this.googleMap = true;
+                    this.$ionicLoading.show({template: '<ion-spinner icon="spiral"></ion-spinner>'});
+                }
+
+                this.GeolocationService.getCurrentLocation().then((position) => {
+                    this.lat = position.coords.latitude;
+                    this.long = position.coords.longitude;
+
+                    this.map.clickedMarker.latitude = this.lat;
+                    this.map.clickedMarker.longitude = this.long;
+                    this.map.zoom = 15;
+                    this.map.center.latitude = this.lat;
+                    this.map.center.longitude = this.long;
+
+                    this.GeolocationService.setGeoPosition(this.map);
+                    this.mapMarkerSet = true;
+                    if (!this.$scope.$$phase) {
+                        this.$scope.$apply();
+                    }
+
+                })
+            }
+        };
 
         static controllerId:string = "GoogleMapCtrl";
     }
