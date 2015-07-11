89fc3a396c642636f7aa44d2bd685c9111bcbb53
T1m1 <Ruprecht.t@gmx.de>
2015-07-11 20:00:54 +0200

    implement pagination for search results

--- www-develop/js/controller/tripOverviewCtrl.ts
+++ www-develop/js/controller/tripOverviewCtrl.ts
@@ -1,24 +1,19 @@
 module Controller {
     export class TripOverviewCtrl {
-        results:any;
+        results:any = [];
         moods:any = [];
         searchView:boolean;
+        page:number = 0;
+        noMoreItemsAvailable:boolean;
 
-        constructor(private $rootScope, private $state, private ResultService, private $stateParams, private TripService,
-                    private UserService, private DataService, private  $ionicLoading, private webPath) {
+        constructor(private $rootScope, private $state, private $stateParams, private TripService, private $scope,
+                    private UserService, private DataService, private  $ionicLoading, private webPath, private SearchService) {
 
             this.$ionicLoading.show({template: '<ion-spinner icon="spiral"></ion-spinner>'});
 
             // if no user id is committed -> controller used for search results
             if (!$stateParams.userId) {
                 this.searchView = true;
-                this.results = ResultService.getResults();
-                $rootScope.$on('newSearchResults', () => {
-                    this.results = ResultService.getResults();
-                    this.$ionicLoading.hide();
-                    this.updateUserInfo();
-                });
-                this.$ionicLoading.hide();
             } else {
                 this.searchView = false;
                 // if user id is comitted as state param user id -> get all trips of user
@@ -68,6 +63,30 @@ module Controller {
 
         numberOfElelementsIn(obj) {
             return Object.keys(obj).length;
+        }
+
+        loadMore() {
+            this.page++;
+            debugger;
+            this.SearchService.getNextTripsFromQuery(this.page).then((result) => {
+                // TODO: fore each element
+                // push to array
+                var arrayLength = result.data.length;
+                for (var i = 0; i < arrayLength; i++) {
+                    this.results.push(result.data[i]);
+                    //Do something
+                }
+                if(arrayLength < 2) {
+                    this.noMoreItemsAvailable = true;
+                }
+                //this.results.push(result.data);
+                this.$ionicLoading.hide();
+                this.updateUserInfo();
+                this.$scope.$broadcast('scroll.infiniteScrollComplete');
+            }).catch((err)=> {
+                this.$ionicLoading.hide();
+            });
+
         }
 
         static controllerId:string = "TripOverviewCtrl";
