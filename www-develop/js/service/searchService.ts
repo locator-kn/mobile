4cc97600907487cd46e93ed11493e21a92ee1e1b
T1m1 <Ruprecht.t@gmx.de>
2015-07-11 20:00:10 +0200

    implement pagination in search trips

--- www-develop/js/service/searchService.ts
+++ www-develop/js/service/searchService.ts
@@ -4,6 +4,12 @@ module Service {
         city:any = {};
         mood:any = {};
 
+        results:any;
+
+        queryPath = this.basePath + '/trips/search';
+        sq:any = {};
+        cityId:string;
+
         constructor(private $http, private $rootScope, private basePath, private lodash, private $ionicLoading) {
         }
 
@@ -11,7 +17,7 @@ module Service {
         getTripsByQuery(searchQuery) {
             // create a copy by value
             var sq = this.lodash.cloneDeep(searchQuery);
-            var query = this.basePath + '/trips/search';
+
             // delete city from query since it is part of the path
             var cityid = sq.city.id;
             delete sq.city;
@@ -20,14 +26,50 @@ module Service {
             moodArray.push(sq.moods.query_name);
             sq.moods = moodArray;
 
+            // save query for pagination -> same query -> other page size
+            this.cityId = cityid;
+            this.sq = sq;
+
             // returning a promise inside a promise will make the outside promise resolving if inside is resolved.
             return this.$http({
-                url: query + '/' + cityid,
+                url: this.queryPath + '/' + cityid,
                 params: sq,
                 method: 'GET'
             });
         }
 
+        setQuery(searchQuery) {
+            // create a copy by value
+            var sq = this.lodash.cloneDeep(searchQuery);
+
+            // delete city from query since it is part of the path
+            var cityid = sq.city.id;
+            delete sq.city;
+            // fix for moods -> currently one mood in array
+            var moodArray = [];
+            moodArray.push(sq.moods.query_name);
+            sq.moods = moodArray;
+
+            // save query for pagination -> same query -> other page size
+            this.cityId = cityid;
+            this.sq = sq;
+        }
+
+        getNextTripsFromQuery(pageNumber, pageSize?) {
+            if (pageNumber) {
+                this.sq.page = pageNumber;
+                if (pageSize) {
+                    this.sq.page_size = pageSize;
+                }
+            }
+
+            // returning a promise inside a promise will make the outside promise resolving if inside is resolved.
+            return this.$http({
+                url: this.queryPath + '/' + this.cityId,
+                params: this.sq,
+                method: 'GET'
+            });
+        }
 
         getTripById(tripId) {
             return this.$http.get(this.basePath + '/trips/' + tripId);
@@ -52,6 +94,16 @@ module Service {
         getMood() {
             return this.mood;
         }
+
+        setResults(results) {
+            this.results = results;
+            this.$rootScope.$emit('newSearchResults');
+        }
+
+        getResults() {
+            return this.results;
+        }
+
 
         static
             serviceId:string = "SearchService";
