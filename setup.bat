a47455cd429af51f1e45a84a5bab4227d00e40d6
T1m1 <Ruprecht.t@gmx.de>
2015-07-11 02:36:48 +0200

    first init of setup script

---
+++ setup.bat
@@ -0,0 +1,8 @@
+ionic add platform android
+
+ionic plugin add cordova-plugin-geolocation
+ionic plugin add cordova-plugin-whitelist
+ionic plugin add org.apache.cordova.camera
+ionic plugin add org.apache.cordova.file
+ionic plugin add org.apache.cordova.file-transfer
+
