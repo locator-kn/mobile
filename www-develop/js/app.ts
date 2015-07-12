a0e2f0f1454b45a7801d62eebffca44e26edeb7a
T1m1 <Ruprecht.t@gmx.de>
2015-07-12 10:23:15 +0200

    add contant

--- www-develop/js/app.ts
+++ www-develop/js/app.ts
@@ -80,6 +80,7 @@ angular.module('starter', deps)
     .constant('basePath', '<%= basePath %>')
     .constant('basePathRealtime', '<%= basePathRealtime %>')
     .constant('webPath', '<%= webPath %>')
+    .constant('maxSpinningDuration', 6000)
 
 
     // controler
