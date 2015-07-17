470d8b5dbd0dd12ce4d6760b973d844891b69db8
T1m1 <Ruprecht.t@gmx.de>
2015-07-17 13:06:00 +0200

    add new states

--- www-develop/js/controller/moodCtrl.ts
+++ www-develop/js/controller/moodCtrl.ts
@@ -7,10 +7,13 @@ module Controller {
         offerState = 'tab.offer';
         moodSearchState = 'tab.search-moods';
         searchState = 'tab.search';
+        editState = 'tab.profile-trip-edit-moods';
+        edit = 'tab.profile-trip-edit';
 
         state:string;
 
-        constructor(private DataService, private SearchService, private $ionicLoading, private $state, private TripService) {
+        constructor(private DataService, private SearchService, private $ionicLoading, private $state, private TripService,
+                    private $stateParams) {
             this.state = this.$state.current.name;
 
             // get all moods from data service
@@ -24,6 +27,12 @@ module Controller {
             if(this.state === this.moodSearchState) {
                 this.SearchService.setMood(mood);
                 this.$state.go(this.searchState);
+            }  else if (this.state === this.editState) {
+                this.TripService.setMood(mood);
+                this.$state.go(this.edit, {
+                    userId: this.$stateParams.userId,
+                    tripId: this.$stateParams.tripId
+                });
             } else {
                 this.TripService.setMood(mood);
                 this.$state.go(this.offerState);
