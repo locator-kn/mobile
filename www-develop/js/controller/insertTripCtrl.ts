c30c52cd7250a5c47dd4f060a87c7e573108069f
T1m1 <Ruprecht.t@gmx.de>
2015-08-03 19:07:46 +0200

    fix date bug

--- www-develop/js/controller/insertTripCtrl.ts
+++ www-develop/js/controller/insertTripCtrl.ts
@@ -186,7 +186,8 @@ module Controller {
                 var currentDate = new Date();
                 var start_date = new Date(this.start_date);
                 var end_date = new Date(this.end_date);
-                if (currentDate > start_date || currentDate > end_date) {
+                if (currentDate.toDateString() > start_date.toDateString() ||
+                    currentDate.toDateString() > end_date.toDateString()) {
                     this.$ionicPopup.alert({title: 'Datum liegt in der Vergangenheit.'});
                     return;
                 }
