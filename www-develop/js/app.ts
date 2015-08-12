427cbcc7740f7ead97725da7eb562881b7775f41
Michael Knoch <info@michaelknoch.de>
2015-08-12 13:43:44 +0200

    show KeyboardAccessoryBar

--- www-develop/js/app.ts
+++ www-develop/js/app.ts
@@ -70,7 +70,7 @@ angular.module('starter', deps)
             // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
             // for form inputs)
             if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
-                cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+                cordova.plugins.Keyboard.hideKeyboardAccessoryBar(false);
             }
 
             // Check for network connection
@@ -97,10 +97,10 @@ angular.module('starter', deps)
                 console.log("Google Analytics Unavailable");
             }
 
-            /*if (window.StatusBar) {
-             // org.apache.cordova.statusbar required
-             StatusBar.styleLightContent();
-             }*/
+            if (window.StatusBar) {
+                // org.apache.cordova.statusbar required
+                StatusBar.styleDefault();
+            }
 
             $rootScope.isAndroid = ionic.Platform.isAndroid();
             $rootScope.isIOS = ionic.Platform.isIOS();
